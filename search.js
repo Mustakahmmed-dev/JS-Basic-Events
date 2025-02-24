d09ead78cc63c4fcc4d0b4a321233dbcb628c914
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-24 20:19:30 +0600

    Boom! solved the issue of the length of the searched elements and leaving elements

--- search.js
+++ search.js
@@ -4,7 +4,6 @@ let resultArea = document.getElementById("result-area");
 document.getElementById("btn-search")
 .addEventListener("click", function(){
     resultArea.style.display = "block";
-    let resultLen = document.getElementById("total-found").value = 0;
 
     let searchingText = document.getElementById("input-search-field").value;
     let searching = searchingText.toLowerCase();
@@ -12,14 +11,20 @@ document.getElementById("btn-search")
     let productsContainer = document.getElementById("products-container");
 
     let products = document.querySelectorAll("#products-container > div");
-    
+        
+    let resultCount = 0;
+    let leavingProducts = 0;
     for(let product of products){
         if(product.innerText.toLowerCase().includes(searching)){
-            productsContainer.append(product);
+            product.style.display = "block";
+
+            resultCount++;
         }
         else{
             product.style.display = "none";
+            leavingProducts++;
         }
     }
-   
+   document.getElementById("total-found").innerText = resultCount;
+   document.getElementById("other-found").innerText = leavingProducts;
 })
